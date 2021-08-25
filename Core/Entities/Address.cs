e7c1d0b6f4638157f3569249ffeabcb800fdff20
Ahassnat <alhassnatabdallah@gmail.com>
2021-08-25 21:30:40 +0300

    Add the class to th core

---
+++ Core/Entities/Address.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Entites
+{
+    public class Address : EntityBase
+    {
+        public string Street { get; set; }
+        public string City { get; set; }
+        public int Number { get; set; }
+    }
+}
---
+++ Core/Entities/EntityBase.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+
+namespace Core.Entites
+{
+   public class EntityBase
+    {
+        public Guid Id { get; set; }
+    }
+}
