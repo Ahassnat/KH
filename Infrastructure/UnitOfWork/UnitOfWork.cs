27a4022fd023c2d06fec496ff26567cc000e546b
Ahassnat <alhassnatabdallah@gmail.com>
2021-08-26 13:52:46 +0300

    Implement Unit Of Work Class

---
+++ Infrastructure/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,32 @@
+﻿using Core.Intefaces;
+using Infrastructure.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.UnitOfWork
+{
+    public class UnitOfWork<T> : IUnitOfWork<T> where T : class
+    {
+        private readonly DataContext _context;
+        private IGenericRepository<T> _entity;
+
+        public UnitOfWork(DataContext context)
+        {
+            _context = context;
+        }
+        public IGenericRepository<T> Entity
+        {
+            get
+            {
+                //?? mean if entity null => initilize new type of GenricRepository
+                return _entity ?? (_entity = new GenericRepository<T>(_context));
+            }
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
