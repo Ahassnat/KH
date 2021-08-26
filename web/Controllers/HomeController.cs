a0093971301f25a900aab4659db801ee6d759377
Ahassnat <alhassnatabdallah@gmail.com>
2021-08-26 14:31:54 +0300

    Making the ViewModel and implent it in the view of index for Owner

--- web/Controllers/HomeController.cs
+++ web/Controllers/HomeController.cs
@@ -2,15 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Core.Entites;
+using Core.Intefaces;
 using Microsoft.AspNetCore.Mvc;
+using web.ViewModels;
 
 namespace web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IUnitOfWork<Owner> _ownerUnitOfWork;
+        private readonly IUnitOfWork<PortfolioItem> _portfolioUnitOfWork;
+
+        public HomeController(IUnitOfWork<Owner> ownerUnitOfWork,
+                              IUnitOfWork<PortfolioItem> portfolioUnitOfWork)
+        {
+            _ownerUnitOfWork = ownerUnitOfWork;
+            _portfolioUnitOfWork = portfolioUnitOfWork;
+        }
         public IActionResult Index()
         {
-            return View();
+            var homeViewModel = new HomeViewModel
+            {
+                Owner = _ownerUnitOfWork.Entity.GetAll().First(),
+                PortfolioItems = _portfolioUnitOfWork.Entity.GetAll().ToList()
+            };
+            return View(homeViewModel);
         }
         public IActionResult About()
         {
--- web/Startup.cs
+++ web/Startup.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Core.Intefaces;
 using Infrastructure;
+using Infrastructure.UnitOfWork;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -29,7 +31,10 @@ namespace web
             services.AddDbContext<DataContext>(option =>
                              option.UseSqlServer(_Config.GetConnectionString("MyConnection")));
 
+
+            services.AddScoped(typeof(IUnitOfWork<>),typeof(UnitOfWork<>));
         }
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
