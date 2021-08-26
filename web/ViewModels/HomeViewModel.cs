1b3541ff534db7b9bfc59ae1724e42653e9ca28d
Ahassnat <alhassnatabdallah@gmail.com>
2021-08-26 14:03:24 +0300

    Intilize the View Models for home controller

---
+++ web/ViewModels/HomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Core.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace web.ViewModels
+{
+    public class HomeViewModel
+    {
+        public Owner Owner { get; set; }
+        public List<PortfolioItem> PortfolioItems { get; set; }
+    }
+}
