5b7a4e858b123558c6e44254b3a94a288bf4f9a4
Ahassnat <alhassnatabdallah@gmail.com>
2021-08-26 13:17:08 +0300

    Intilize the Generic Inteface Reposotry and Unit of work in Core project

---
+++ Core/Intefaces/IGenericRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Intefaces
+{
+    public interface IGenericRepository<T> where T: class   
+    {
+        IEnumerable<T> GetAll();
+        T GetByID(object id);
+        void Insert(T entity);
+        void Update(T entity);
+        void Delete(object id);
+    }
+}
